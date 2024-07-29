d6a778a30e95438b44c5bbedc4614c3f9c5c091f
Rafael Ignacio Zurita <rafa@fi.uncoma.edu.ar>
2024-07-29 09:37:16 -0300

    cambios

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
  go build -o libserver.so -buildmode=c-shared server.go
   g++ -o tree_extractor c2.cpp `pkg-config --cflags --libs opencv4`
   gcc -o imghash imghash.c -L. -lserver
-  LD_LIBRARY_PATH=/lib:/usr/lib/:/home/rafa/programacion/cand_doctorado/loop-detection/imgdiff2/imgsim/   ./imghash
+  LD_LIBRARY_PATH=/lib:/usr/lib/:/home/rafa/programacion/cand_doctorado/loop-detection/imgdiff2/imgfingerprint/   ./imghash
